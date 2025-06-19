3a75698089af2c12f76e570d7f00f4e7748a564e
mityu <mityu.mail@gmail.com>
2025-06-19 21:52:22 +0900

    refactor: Change variable names

--- denops/@ddu-filters/matcher_multi_regex.ts
+++ denops/@ddu-filters/matcher_multi_regex.ts
@@ -65,14 +65,14 @@ export class Filter extends BaseFilter<Params> {
 
     // Calculate matched position below.
     const pushHighlightInfo = (
-      item: { highlights: ItemHighlight[]; },
-      matchedText: string,
+      item: { highlights: ItemHighlight[] },
+      matcherText: string,
       match: RegExpExecArray,
     ) => {
       item.highlights.push({
         name: "matched",
         hl_group: args.filterParams.highlightMatched,
-        col: strlen(matchedText.slice(0, match.index)) + 1,
+        col: strlen(matcherText.slice(0, match.index)) + 1,
         width: strlen(match[0]),
       });
     };
@@ -81,22 +81,22 @@ export class Filter extends BaseFilter<Params> {
       (items, re) => {
         if (args.filterParams.highlightGreedy) {
           items.map((item) => {
-            const displayText = item.display ?? item.matcherKey;
+            const hlMatcherText = item.display ?? item.matcherKey;
             const r = new RegExp(re, re.flags + "g");
             let prevMatchIndex = -1;
             for (;;) { // Search for all the matches.
-              const match = r.exec(displayText);
+              const match = r.exec(hlMatcherText);
               if (!match || match.index === prevMatchIndex) {
                 break;
               }
-              pushHighlightInfo(item, displayText, match);
+              pushHighlightInfo(item, hlMatcherText, match);
               prevMatchIndex = match.index;
             }
           });
         } else {
           items.map((item) => {
-            const displayText = item.display ?? item.matcherKey;
-            pushHighlightInfo(item, displayText, re.exec(displayText)!);
+            const hlMatcherText = item.display ?? item.matcherKey;
+            pushHighlightInfo(item, hlMatcherText, re.exec(hlMatcherText)!);
           });
         }
         return items;
